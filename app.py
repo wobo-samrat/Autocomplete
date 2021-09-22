db293b7a588032da4d17143eb9d129ff4fdf3d88
wobo-samrat <82931916+wobo-samrat@users.noreply.github.com>
2021-09-22 12:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 
 
 model_dir = os.getcwd() + '/model/'
+print("model dir is :: ",model_dir)
 tokenizer = T5Tokenizer.from_pretrained('t5-small')
 model = T5ForConditionalGeneration.from_pretrained(model_dir)
 model.resize_token_embeddings(len(tokenizer))
