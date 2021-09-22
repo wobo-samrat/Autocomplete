356e52fbcb7c34f44b87208ccf52f29dab0a8bbc
wobo-samrat <82931916+wobo-samrat@users.noreply.github.com>
2021-09-22 10:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
-import pickle
 import pandas as pd
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
